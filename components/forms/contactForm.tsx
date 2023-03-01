08fb2e74ded028b7be9d360e675d916bdf1019f8
Andrew Morton <mortocks@gmail.com>
2023-03-01 16:25:09 +1000

    form outline remove

--- components/forms/contactForm.tsx
+++ components/forms/contactForm.tsx
@@ -37,7 +37,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         <input
           ref={ref}
           {...rest}
-          className="bg-transparent py-4 text-large border-0 border-b-2 text-lg focus:outline-0 focus:border-primary dark:focus:border-gold transition-all placeholder:text-neutral-200 "
+          className="bg-transparent py-4 text-large border-0 border-b-2 text-lg outline-0 focus:outline-0 focus:border-primary dark:focus:border-gold transition-all placeholder:text-neutral-200 "
         />
         {error && (
           <span className="text-error pt-3 font-bold font-mono">{error}</span>
@@ -56,7 +56,7 @@ const TextArea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
         <TextareaAutosize
           minRows={1}
           ref={ref}
-          className="bg-transparent py-4 text-large border-0 border-b text-lg  focus:outline-0 focus:border-primary  dark:focus:border-gold transition-all placeholder:text-neutral-200 scrollbar-hide"
+          className="bg-transparent py-4 text-large border-0 border-b text-lg outline-0 focus:outline-0 focus:border-primary  dark:focus:border-gold transition-all placeholder:text-neutral-200 scrollbar-hide"
           {...rest}
         />
 
