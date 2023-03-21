3f6b21096f220c06d5e491ef6c05c4f25e944288
Andrew Morton <mortocks@gmail.com>
2023-03-21 15:47:34 +1000

    improved blog layout

--- pages/insights/[slug].tsx
+++ pages/insights/[slug].tsx
@@ -8,23 +8,6 @@ import Link from "next/link";
 import Tags from "../../components/Tags";
 import { remark } from "remark";
 import html from "remark-html";
-import { useEffect } from "react";
-import Prism from "prismjs";
-import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
-import tsx from "react-syntax-highlighter/dist/cjs/languages/prism/tsx";
-import typescript from "react-syntax-highlighter/dist/cjs/languages/prism/typescript";
-import scss from "react-syntax-highlighter/dist/cjs/languages/prism/scss";
-import bash from "react-syntax-highlighter/dist/cjs/languages/prism/bash";
-import markdown from "react-syntax-highlighter/dist/cjs/languages/prism/markdown";
-import json from "react-syntax-highlighter/dist/cjs/languages/prism/json";
-import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
-
-SyntaxHighlighter.registerLanguage("tsx", tsx);
-SyntaxHighlighter.registerLanguage("typescript", typescript);
-SyntaxHighlighter.registerLanguage("scss", scss);
-SyntaxHighlighter.registerLanguage("bash", bash);
-SyntaxHighlighter.registerLanguage("markdown", markdown);
-SyntaxHighlighter.registerLanguage("json", json);
 
 interface PageProps {
   post: Post;
@@ -36,54 +19,6 @@ interface Params extends ParsedUrlQuery {
 }
 
 const BlogPost: NextPage<PageProps> = ({ post, _html }) => {
-  const syntaxTheme = oneDark;
-
-  const MarkdownComponents: object = {
-    code({ node, inline, className, ...props }) {
-      const hasLang = /language-(\w+)/.exec(className || "");
-      const hasMeta = node?.data?.meta;
-
-      const applyHighlights: object = (applyHighlights: number) => {
-        if (hasMeta) {
-          const RE = /{([\d,-]+)}/;
-          const metadata = node.data.meta?.replace(/\s/g, "");
-          const strlineNumbers = RE?.test(metadata)
-            ? RE?.exec(metadata)[1]
-            : "0";
-          const highlightLines = rangeParser(strlineNumbers);
-          const highlight = highlightLines;
-          const data: string = highlight.includes(applyHighlights)
-            ? "highlight"
-            : null;
-          return { data };
-        } else {
-          return {};
-        }
-      };
-
-      return hasLang ? (
-        <SyntaxHighlighter
-          style={syntaxTheme}
-          language={hasLang[1]}
-          PreTag="div"
-          className="codeStyle"
-          showLineNumbers={true}
-          wrapLines={hasMeta}
-          useInlineStyles={true}
-          lineProps={applyHighlights}
-        >
-          BOOP
-          {props.children}
-        </SyntaxHighlighter>
-      ) : (
-        <>
-          Boop
-          <code className={className} {...props} />
-        </>
-      );
-    },
-  };
-
   return (
     <Layout>
       <header className="flex flex-col pt-40 py-10 justify-center container max-w-4xl">
