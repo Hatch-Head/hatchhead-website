4baf6911cd242c568de16876c8bc4e37df50f82f
Andrew Morton <mortocks@gmail.com>
2023-03-22 00:30:51 +1000

    tag hover styles

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const Tags = ({ tags = [], center = true, linkPrefix }: Props) => {
   const style =
-    "font-mono z-1 relative mb-4 border-2 border-gray-900 dark:border-gray-50 flex items-center px-4 py-1 text-xs transition duration-150 ease-out rounded-full transform focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 whitespace-nowrap text-gray-900 grow-0 dark:text-gray-50";
+    "font-mono z-1 relative mb-4 border-2 border-gray-900 dark:border-gray-50 flex items-center px-4 py-1 text-xs transition duration-150 ease-out rounded-full transform focus:shadow-outline focus:outline-none focus:ring-0 ring-offset-current ring-offset-2 whitespace-nowrap text-gray-900 grow-0 dark:text-gray-50";
 
   return (
     <div
@@ -23,7 +23,7 @@ const Tags = ({ tags = [], center = true, linkPrefix }: Props) => {
           <Link
             key={tag}
             href={`/${linkPrefix}/${slug}`}
-            className={style}
+            className={`${style} hover:bg-black hover:text-white dark dark:hover:bg-white dark:hover:text-black`}
             suppressHydrationWarning
           >
             {tag}
