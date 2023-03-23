7743b22f90c6283205fb4d7cbe2012ec36bb7549
Andrew Morton <mortocks@gmail.com>
2023-03-23 16:18:39 +1000

    ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,25 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ParallaxProvider } from "react-scroll-parallax";
-import { MDXProvider } from "@mdx-js/react";
-
+import Script from "next/script";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ParallaxProvider>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-P8M72T0GEL`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-P8M72T0GEL');
+          `}
+      </Script>
+
       <Component {...pageProps} />
     </ParallaxProvider>
   );
