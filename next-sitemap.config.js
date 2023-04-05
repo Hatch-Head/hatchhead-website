c8d81652865fc5705e46999e819c8952edd7d019
Andrew Morton <mortocks@gmail.com>
2023-04-06 09:14:07 +1000

    sitemap base url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://hatchhead.co",
+  siteUrl: process.env.URL || "https://hatchhead.co",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
