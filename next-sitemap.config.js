19549c7df169d92f31040bb229110b9b10783be1
Andrew Morton <mortocks@gmail.com>
2023-04-06 09:14:51 +1000

    comment re netlify

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
+  // Netlify variables https://docs.netlify.com/configure-builds/environment-variables/
   siteUrl: process.env.URL || "https://hatchhead.co",
   generateRobotsTxt: true, // (optional)
   // ...other options
