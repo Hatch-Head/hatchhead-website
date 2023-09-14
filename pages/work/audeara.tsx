f8465ac9a16c9385464cede4ae0aeeb57260c419
Andrew Morton <mortocks@gmail.com>
2023-09-14 15:14:22 +1000

    final audeara assets and quote

--- pages/work/audeara.tsx
+++ pages/work/audeara.tsx
@@ -14,6 +14,7 @@ import ScreensWithHeadphones from "../../assets/img/audeara-screens-with-headpho
 import Audiogram from "../../assets/img/audeara-working-audiograms.png";
 import CardVideo1 from "../../assets/vid/audeara-card-audio-source.mp4";
 import CardVideo2 from "../../assets/vid/audeara-card-power-cable.mp4";
+import CardVideo3 from "../../assets/vid/audeara-card-audio-cable.mp4";
 import HearingCheckVid from "../../assets/vid/audeara-hearing-check.mp4";
 import ScreenPersonalisationVid from "../../assets/vid/audeara-screen-personalisation.mp4";
 import FadeUp from "../../components/transitions/FadeUp";
@@ -65,14 +66,25 @@ const Audeara = () => (
             Delivering perfect sound with Audeara's mobile app.
           </h1>
 
-          <p className="text-xl text-white mb-12">Audeara, a leader in hearing-health technology, create products that calibrate thier audio devices to your hearing so you experience clearer, brighter sounds and offset damaged or poor hearing.</p>
+          <p className="text-xl text-white mb-12">
+            Audeara, a leader in hearing-health technology, create products that
+            calibrate thier audio devices to your hearing so you experience
+            clearer, brighter sounds and offset damaged or poor hearing.
+          </p>
 
           <p className="text-xl text-white mb-12">
-            Audeara pivoted away from the mainstream consumer-audio market and focused on hearing wellness. Those that were hard of hearing - a typical byproduct of getting older - we're now the ideal customers for Audeara's audio products.
+            Audeara pivoted away from the mainstream consumer-audio market and
+            focused on hearing wellness. Those that were hard of hearing - a
+            typical byproduct of getting older - we're now the ideal customers
+            for Audeara's audio products.
           </p>
-          <p className="text-xl text-white"> This new direction also meant a new visual identity. This sparked an overhaul of their flagship mobile app. For Hatch Head, our goal was to redesign the mobile app experience for those not familiar with cutting edge technology, to deliver the ability to plug-in</p>
-
-
+          <p className="text-xl text-white">
+            {" "}
+            This new direction also meant a new visual identity. This sparked an
+            overhaul of their flagship mobile app. For Hatch Head, our goal was
+            to redesign the mobile app experience for those not familiar with
+            cutting edge technology, to deliver the ability to plug-in
+          </p>
         </div>
       </header>
 
@@ -133,42 +145,72 @@ const Audeara = () => (
           <div className="md:basis-2/3 mb-24">
             <h2>The challenge</h2>
             <p>
-              Working closely with the team, we recognised the need to reconfigure the design of every screen. This wasn't designed for audiophiles who appreciate intricate sliders and buttons. Quite the opposite. Users needed to be able to connect, calibrate, and start listening in under 2 minutes.
+              Working closely with the team, we recognised the need to
+              reconfigure the design of every screen. This wasn't designed for
+              audiophiles who appreciate intricate sliders and buttons. Quite
+              the opposite. Users needed to be able to connect, calibrate, and
+              start listening in under 2 minutes.
             </p>
 
             <p>
-              The directive for the app was clear: emphasise simplicity. As a result of its evolution towards a broader consumer base, our challenge was to refocus its core purpose. This became an exercise in minimalism — fewer features, less confusion, and reduced time spent navigating. Accessibility was paramount; we needed larger fonts and improved colour contrast to ensure readability. Our labels had to be precise, descriptive, yet approachable.
+              The directive for the app was clear: emphasise simplicity. As a
+              result of its evolution towards a broader consumer base, our
+              challenge was to refocus its core purpose. This became an exercise
+              in minimalism — fewer features, less confusion, and reduced time
+              spent navigating. Accessibility was paramount; we needed larger
+              fonts and improved colour contrast to ensure readability. Our
+              labels had to be precise, descriptive, yet approachable.
             </p>
-
           </div>
         </div>
       </section>
 
       <section className="bg-neutral-10 md:py-24">
         <div className="container max-w-5xl px-6 mx-auto prose dark:prose-dark">
-
           <h1 className="text-xl font-bold">Opportunity</h1>
           <p>Audeara's technology is backed by</p>
 
           <p>
-            Onboarding – the process of guiding users to that 'a-ha' moment – presented another challenge. Audeara's headphones perform brilliantly even without personalisation. However, they genuinely shine when tailored to an individual's hearing. We devoted significant time to streamlining the headphones, focusing on both the hardware and software, to ensure users received the best possible experience. considered integrating directly with audio clinics, pre-loading hearing calibrations sourced from audiograms, and deploying them straight into the headphones.
+            Onboarding – the process of guiding users to that 'a-ha' moment –
+            presented another challenge. Audeara's headphones perform
+            brilliantly even without personalisation. However, they genuinely
+            shine when tailored to an individual's hearing. We devoted
+            significant time to streamlining the headphones, focusing on both
+            the hardware and software, to ensure users received the best
+            possible experience. considered integrating directly with audio
+            clinics, pre-loading hearing calibrations sourced from audiograms,
+            and deploying them straight into the headphones.
           </p>
 
           <h1 className="text-xl font-bold">From Open to On ASAP</h1>
           <p>
-            At the core of the app is the calibration of the user's hearing to the headphones. A hearing test typically takes between 5 to 7 minutes. While this might seem lengthy, the result is a highly accurate representation of one's hearing. The real 'a-ha' moment for users is the subsequent personalisation demo. Switching from the default profile to the personalised one can be a revelation. Suddenly, activities like listening to music, having conversations, and watching television become possible again. We sought to provide a variety of audio playback options, including support for Spotify and other external players.
+            At the core of the app is the calibration of the user's hearing to
+            the headphones. A hearing test typically takes between 5 to 7
+            minutes. While this might seem lengthy, the result is a highly
+            accurate representation of one's hearing. The real 'a-ha' moment for
+            users is the subsequent personalisation demo. Switching from the
+            default profile to the personalised one can be a revelation.
+            Suddenly, activities like listening to music, having conversations,
+            and watching television become possible again. We sought to provide
+            a variety of audio playback options, including support for Spotify
+            and other external players.
           </p>
 
           <h1 className="text-xl font-bold">Hearing checks</h1>
           <p>
-            Clinical diagnosis can create some complexity in how the user interprets their hearing. It's one thing to have boosted sound, and another entirely to see that your 7K range is hitting at -20DB. Although it wasn't our primary goal, we believed translating the state of the person's hearing offered a valuable opportunity get a better understanding of their overall hearing picture.
+            Clinical diagnosis can create some complexity in how the user
+            interprets their hearing. It's one thing to have boosted sound, and
+            another entirely to see that your 7K range is hitting at -20DB.
+            Although it wasn't our primary goal, we believed translating the
+            state of the person's hearing offered a valuable opportunity get a
+            better understanding of their overall hearing picture.
           </p>
           <p>
-            We wanted to motivate users to re-assess their hearing every six months. This would ensure their headphones remained synchronised with any changes in their hearing capabilities. It may seem minor, but such adjustments can have significant long-term effects.
+            We wanted to motivate users to re-assess their hearing every six
+            months. This would ensure their headphones remained synchronised
+            with any changes in their hearing capabilities. It may seem minor,
+            but such adjustments can have significant long-term effects.
           </p>
-
-
-
 
           <Image
             src={Audiogram}
@@ -242,7 +284,7 @@ const Audeara = () => (
             </FadeUp>
             <FadeUp>
               <video
-                src={CardVideo1}
+                src={CardVideo3}
                 width={390}
                 height={467}
                 className="w-fill rounded-xl shadow-lg border border-gray-50 dark:border-0"
@@ -255,10 +297,19 @@ const Audeara = () => (
           </div>
         </div>
       </section>
-
-
+      <FadeUp>
+        <Testimonial
+          className="py-24"
+          image={Alex}
+          name="Alex Afflick"
+          role="CTO"
+          quote="Hatch Head complement our team's capability with their knowledge of
+        digital design and user experience. It's a pleasure to have access to
+        them, where they that can jump in and work to deliver great outcomes."
+        />
+      </FadeUp>
     </Layout>
-  </AnimatePresence >
+  </AnimatePresence>
 );
 
 export default Audeara;
