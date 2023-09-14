b887e8edbcb798324ef01d49e27a4cd30f0f0b7a
Brent Wallace <brentwallac@gmail.com>
2023-09-14 11:19:41 +1000

    Updated Audeara text

--- pages/work/audeara.tsx
+++ pages/work/audeara.tsx
@@ -50,17 +50,19 @@ const Audeara = () => (
         </FadeUp>
 
         <div className="relative container px-6 mx-auto z-30 max-w-6xl">
-          <h1 className="text-3xl text-white font-bold mb-8">Audeara app</h1>
-          <h1 className="text-2xl text-white mb-12">
-            Scaling up into a new market
+          <h1 className="text-3xl text-white font-bold mb-8">Now Hear This.</h1>
+          <h1 className="text-2xl text-white mb-12 ">
+            Delivering perfect sound with Audeara's mobile app.
           </h1>
-          <p className="text-xl text-white">
-            Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis
-            vestibulum. Maecenas faucibus mollis interdum. Etiam porta sem
-            malesuada magna mollis euismod. Sed posuere consectetur est at
-            lobortis. Morbi leo risus, porta ac consectetur ac, vestibulum at
-            eros.
+
+          <p className="text-xl text-white mb-12">Audeara, a leader in hearing-health technology, create products that calibrate thier audio devices to your hearing so you experience clearer, brighter sounds and offset damaged or poor hearing.</p>
+
+          <p className="text-xl text-white mb-12">
+            Audeara pivoted away from the mainstream consumer-audio market and focused on hearing wellness. Those that were hard of hearing - a typical byproduct of getting older - we're now the ideal customers for Audeara's audio products.
           </p>
+          <p className="text-xl text-white"> This new direction also meant a new visual identity. This sparked an overhaul of their flagship mobile app. For Hatch Head, our goal was to redesign the mobile app experience for those not familiar with cutting edge technology, to deliver the ability to plug-in</p>
+
+
         </div>
       </header>
 
@@ -119,42 +121,42 @@ const Audeara = () => (
           </div>
 
           <div className="md:basis-2/3 mb-24">
-            <h2>Problem stuff</h2>
+            <h2>The challenge</h2>
             <p>
-              Maecenas sed diam eget risus varius blandit sit amet non magna.
-              Cum sociis natoque penatibus et magnis dis parturient montes,
-              nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor
-              fringilla.
+              Working closely with the team, we recognised the need to reconfigure the design of every screen. This wasn't designed for audiophiles who appreciate intricate sliders and buttons. Quite the opposite. Users needed to be able to connect, calibrate, and start listening in under 2 minutes.
             </p>
+
             <p>
-              Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque
-              nisl consectetur et. Fusce dapibus, tellus ac cursus commodo,
-              tortor mauris condimentum nibh, ut fermentum massa justo sit amet
-              risus. Maecenas sed diam eget risus varius blandit sit amet non
-              magna.
+              The directive for the app was clear: emphasise simplicity. As a result of its evolution towards a broader consumer base, our challenge was to refocus its core purpose. This became an exercise in minimalism — fewer features, less confusion, and reduced time spent navigating. Accessibility was paramount; we needed larger fonts and improved colour contrast to ensure readability. Our labels had to be precise, descriptive, yet approachable.
             </p>
+
           </div>
         </div>
       </section>
 
       <section className="bg-neutral-10 md:py-24">
         <div className="container max-w-5xl px-6 mx-auto prose dark:prose-dark">
-          <h1 className="text-xl font-bold">Opportunity stuff</h1>
-          <p>
-            Maecenas sed diam eget risus varius blandit sit amet non magna. Cum
-            sociis natoque penatibus et magnis dis parturient montes, nascetur
-            ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla.
-            Nullam id dolor id nibh ultricies vehicula ut id elit.
-          </p>
+          <h1 className="text-xl font-bold">Onboarding</h1>
 
           <p>
-            Sed posuere consectetur est at lobortis. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit. Donec sed odio dui. Donec sed odio dui.
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-            Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum
-            nibh, ut fermentum massa justo sit amet risus. Maecenas sed diam
-            eget risus varius blandit sit amet non magna.
+            Onboarding – the process of guiding users to that 'a-ha' moment – presented another challenge. Audeara's headphones perform brilliantly even without personalisation. However, they genuinely shine when tailored to an individual's hearing. We devoted significant time to streamlining the headphones, focusing on both the hardware and software, to ensure users received the best possible experience. considered integrating directly with audio clinics, pre-loading hearing calibrations sourced from audiograms, and deploying them straight into the headphones.
           </p>
+
+          <h1 className="text-xl font-bold">From Open to On ASAP</h1>
+          <p>
+            At the core of the app is the calibration of the user's hearing to the headphones. A hearing test typically takes between 5 to 7 minutes. While this might seem lengthy, the result is a highly accurate representation of one's hearing. The real 'a-ha' moment for users is the subsequent personalisation demo. Switching from the default profile to the personalised one can be a revelation. Suddenly, activities like listening to music, having conversations, and watching television become possible again. We sought to provide a variety of audio playback options, including support for Spotify and other external players.
+          </p>
+
+          <h1 className="text-xl font-bold">Hearing checks</h1>
+          <p>
+            Clinical diagnosis can create some complexity in how the user interprets their hearing. It's one thing to have boosted sound, and another entirely to see that your 7K range is hitting at -20DB. Although it wasn't our primary goal, we believed translating the state of the person's hearing offered a valuable opportunity get a better understanding of their overall hearing picture.
+          </p>
+          <p>
+            We wanted to motivate users to re-assess their hearing every six months. This would ensure their headphones remained synchronised with any changes in their hearing capabilities. It may seem minor, but such adjustments can have significant long-term effects.
+          </p>
+
+
+
 
           <Image
             src={Audiogram}
@@ -242,15 +244,9 @@ const Audeara = () => (
         </div>
       </section>
 
-      <Testimonial
-        className="py-24 bg-white dark:bg-black"
-        name="Alex"
-        role="CTO"
-        quote="As a large language model, I am delighted to pen down my journey with Hatch Head. Collaborating with this dynamic team to conceptualise and design their business has been nothing short of an inspiring journey, filled with bursts of creativity, innovation, and mutual growth."
-        image={Alex}
-      />
+
     </Layout>
-  </AnimatePresence>
+  </AnimatePresence >
 );
 
 export default Audeara;
