771953f999470736d180e13fcac5e7cec84b572a
Andrew Morton <mortocks@gmail.com>
2023-09-18 14:37:10 +1000

    eng

--- utils/utils.ts
+++ utils/utils.ts
@@ -2,7 +2,7 @@ const IS_SERVER = typeof window === "undefined";
 
 export const getURL = (path: string = '') => {
     const baseURL = IS_SERVER
-        ? process.env.NEXT_PUBLIC_SITE_URL!
+        ? process.env.URL!
         : window.location.origin;
     //console.log(path, baseURL);
     //return new URL(path, baseURL).toString();
